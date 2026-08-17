c9e1a29dd45d154392ab8c3cabd0c89fbd3f56cd
jonlinnet28 <jonlinnet28@gmail.com>
2026-08-17 11:04:02 +0200

    more changes to delete

--- src/Buttons/DeletePostButton.tsx
+++ src/Buttons/DeletePostButton.tsx
@@ -2,6 +2,6 @@ import type {MyChildComponentProps} from "@/posts/Post.tsx";
 
 export function DeletePostButton({posts, removePost, }: MyChildComponentProps) {
 
-    return <div>the Post is: {posts?.title} <button onClick={() => removePost(posts.id)}>delete this stuff</button>
+    return <div>the Post is: {posts?.title} <button onClick={() => removePost(posts.id)}>Delete</button>
     </div>
 }
